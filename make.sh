f3194c13a922f0fa1aec941ee353ad11a48ffada
Qi Zhang <dg1628023@smail.nju.edu.cn>
2018-12-31 09:38:15 +0800

    Create make.sh

---
+++ make.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd libsrc
+mpiifort -O3 -xAVX -c -traceback -g module_calculate_tbb.f90 -I${CRTM}/include
+mpiifort -O3 -xAVX -c -traceback -g main.f90 -I${NETCDF}/include -I${HDF5}/include -I${ZLIB}/include -I${CRTM}/include
+cd ../bin
+mpiifort -xAVX -traceback -g ../libsrc/module_calculate_tbb.o ../libsrc/main.o -o CRTM.exe -L${NETCDF}/lib -lnetcdff -lnetcdf -L${HDF5}/lib -lhdf5_hl -lhdf5 -L${ZLIB}/zlib -lz -L${CRTM}/lib -lcrtm
